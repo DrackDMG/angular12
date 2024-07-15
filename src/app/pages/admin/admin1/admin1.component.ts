b782896349313f096cf4d6ef79d6431bc985a591
Daniel Morales <danielmoralesgonzalez5@gmail.com>
2024-07-15 12:37:54 -0600

    Uso de Pipes

--- src/app/pages/admin/admin1/admin1.component.ts
+++ src/app/pages/admin/admin1/admin1.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { BehaviorSubject, Observable } from "rxjs";
 
 @Component({
-  selector: 'app-admin1',
-  templateUrl: './admin1.component.html',
-  styleUrls: ['./admin1.component.scss']
+  selector: "app-admin1",
+  templateUrl: "./admin1.component.html",
+  styleUrls: ["./admin1.component.scss"],
 })
 export class Admin1Component implements OnInit {
+  date: Date = new Date();
+  auxJson = { name: "Juan", age: 20 };
+  auxObserver$ = new BehaviorSubject<number[]>([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    setInterval(() => {
+      this.date = new Date();
+    }, 1000);
   }
 
+  ngOnInit(): void {}
+
+  onAdd() {
+    this.auxObserver$.next([
+      ...this.auxObserver$.value,
+      this.auxObserver$.value.length + 1,
+    ]);
+  }
 }
