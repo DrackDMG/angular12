ee7358973008727b088ee6f474303d36593bfd55
Daniel Morales <danielmoralesgonzalez5@gmail.com>
2024-07-15 12:19:59 -0600

    Forma de trabajo con modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,10 @@ import { FormsModule } from "@angular/forms";
 import { Componet3Component } from "./componet3/componet3.component";
 import { Directive1Directive } from "./directive1.directive";
 import { Directive2Directive } from "./directive2.directive";
+import { HomeModule } from "./pages/home/home.module";
+import { AdminModule } from "./pages/admin/admin.module";
+import { BuyModule } from "./pages/buy/buy.module";
+import { SharedModule } from "./shared/shared.module";
 
 @NgModule({
   declarations: [
@@ -18,7 +22,14 @@ import { Directive2Directive } from "./directive2.directive";
     Directive1Directive,
     Directive2Directive,
   ],
-  imports: [BrowserModule, FormsModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HomeModule,
+    AdminModule,
+    BuyModule,
+    SharedModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { LoaderComponent } from "./loader/loader.component";
+import { CardComponent } from "./card/card.component";
+
+@NgModule({
+  declarations: [LoaderComponent, CardComponent],
+  imports: [CommonModule],
+  exports: [LoaderComponent, CardComponent],
+})
+export class SharedModule {}
