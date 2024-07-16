a852e05fc5fef18e21d60394e3c130c76dacbea4
Daniel Morales <danielmoralesgonzalez5@gmail.com>
2024-07-15 19:04:24 -0600

    Metodo con paramtros, Nullish Coalescing y or

--- src/app/pages/admin/admin2/admin2.component.ts
+++ src/app/pages/admin/admin2/admin2.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-admin2',
-  templateUrl: './admin2.component.html',
-  styleUrls: ['./admin2.component.scss']
+  selector: "app-admin2",
+  templateUrl: "./admin2.component.html",
+  styleUrls: ["./admin2.component.scss"],
 })
 export class Admin2Component implements OnInit {
+  name = "John";
+  name2 = 0;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  /**
+   * @param name
+   * @param lastName
+   * @returns
+   */
+  public testParams(name?: string, lastName?: string): string {
+    const nameaux = "John";
+    const lastNameaux = "Doe";
+    console.log("name", name ?? nameaux, "lastName", lastName ?? lastNameaux);
+    return `Hello ${name} ${lastName}`;
   }
-
 }
