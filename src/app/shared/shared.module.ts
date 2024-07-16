b30ee0c3b331ae62b9d1d2858f84492fc288a970
Daniel Morales <danielmoralesgonzalez5@gmail.com>
2024-07-15 19:24:17 -0600

    Creacion de un pipe propio

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { LoaderComponent } from "./loader/loader.component";
 import { CardComponent } from "./card/card.component";
+import { ExponentPipe } from "./exponent.pipe";
 
 @NgModule({
-  declarations: [LoaderComponent, CardComponent],
+  declarations: [LoaderComponent, CardComponent, ExponentPipe],
   imports: [CommonModule],
-  exports: [LoaderComponent, CardComponent],
+  exports: [LoaderComponent, CardComponent, ExponentPipe],
 })
 export class SharedModule {}
