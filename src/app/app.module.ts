58cb083bcdad1125ab6bd54d91d57aa23e445617
Daniel Morales <danielmoralesgonzalez5@gmail.com>
2024-07-16 20:12:07 -0600

    creacion de los componentes y configuracion de las rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { RouterModule, Routes } from "@angular/router";
 
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "login",
+    pathMatch: "full",
+  },
+  {
+    path: "login",
+    loadChildren: () =>
+      import("./login/login.module").then((m) => m.LoginModule),
+  },
+  {
+    path: "pages",
+    loadChildren: () =>
+      import("./pages/pages.module").then((m) => m.PagesModule),
+  },
+];
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,17 +1,17 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { ProfileComponent } from "./profile/profile.component";
+import { LoginComponent } from "./login/login.component";
 import { RouterModule, Routes } from "@angular/router";
 
 const routes: Routes = [
   {
     path: "",
-    component: ProfileComponent,
+    component: LoginComponent,
   },
 ];
 
 @NgModule({
-  declarations: [ProfileComponent],
+  declarations: [LoginComponent],
   imports: [CommonModule, RouterModule.forChild(routes)],
 })
-export class ProfileModule {}
+export class LoginModule {}
