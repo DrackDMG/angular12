a7e39a00895db45f68c92acf6f193fe5e775cdb7
Daniel Morales <danielmoralesgonzalez5@gmail.com>
2024-07-17 10:25:07 -0600

    Acomodo de la estructura de carpetas y archivos

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,11 +1,11 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { LoginComponent } from "./login/login.component";
-import { RouterModule, Routes } from "@angular/router";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login.component';
+import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: "",
+    path: '',
     component: LoginComponent,
   },
 ];
